5e676cf3e6adac8fe4f698fc1338134c1b0dd9a9
laravelshao <laravelshao@163.com>
2022-07-24 14:52:00 +0800

    使用线程池执行同步 batch 至其它注册服务端

--- src/main/java/com/cl/register/server/cluster/PeersReplicator.java
+++ src/main/java/com/cl/register/server/cluster/PeersReplicator.java
@@ -7,6 +7,8 @@ import com.cl.register.server.web.RegisterRequest;
 
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -139,15 +141,22 @@ public class PeersReplicator {
      */
     class PeersReplicateThread extends Thread {
 
+        ExecutorService threadPool = Executors.newFixedThreadPool(RegisterServerCluster.getPeers().size());
+
         @Override
         public void run() {
             while (true) {
                 try {
                     PeersReplicateBatch batch = replicateQueue.take();
                     if (batch != null) {
-                        // 遍历所有的其他的register-server地址
-                        // 给每个地址的register-server都发送一个http请求同步batch
-                        System.out.println("给所有其他的register-server发送请求，同步batch过去......");
+
+                        for (String peer : RegisterServerCluster.getPeers()) {
+                            threadPool.execute(() -> {
+                                // 遍历所有的其他的 register-server 地址
+                                // 给每个地址的register-server都发送一个http 请求同步 batch
+                                System.out.println("给" + peer + "发送请求，同步batch过去......");
+                            });
+                        }
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
---
+++ src/main/java/com/cl/register/server/cluster/RegisterServerCluster.java
@@ -0,0 +1,24 @@
+package com.cl.register.server.cluster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 注册服务端集群
+ *
+ * @author qinghua.shao
+ * @date 2022/7/24
+ * @since 1.0.0
+ */
+public class RegisterServerCluster {
+
+    private static List<String> peers = new ArrayList<>();
+
+    static {
+        // 读取配置文件，看看你配合了哪些机器部署的 register-server
+    }
+
+    public static List<String> getPeers() {
+        return peers;
+    }
+}
